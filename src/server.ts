6fea9418447b24cc47e4535b6f0ed371c0fb410e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-03-15 21:49:45 -0300

    chore: add debug config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.send('Hello World');
+});
+
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
