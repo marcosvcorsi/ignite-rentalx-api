bf765f9bc3bf76a58d55b5e2b36725426650153e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-03-17 22:06:55 -0300

    docs: add swagger config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
+import swaggerConfigFile from './docs/swagger.json';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfigFile));
 
 app.use(routes);
 
