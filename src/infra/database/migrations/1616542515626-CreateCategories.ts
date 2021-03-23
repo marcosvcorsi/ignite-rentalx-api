30c63492ce1840e280b4f611ff0a496cff9fb98c
Marcos Corsi <marcosvcorsi@gmail.com>
2021-03-23 20:41:16 -0300

    feat: add migration config and create categories

---
+++ src/infra/database/migrations/1616542515626-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategories1616542515626 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.dropTable('categories');
+  }
+}
